f571f9342f33089f596c154c6e5aed6ca85e7c47
Joss Dixon <josscdixon@gmail.com>
2021-11-10 12:06:32 +0900

    go

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'HAL is obliterating all restaurants...'
+Restaurant.destroy_all
+puts 'HAL is fabricating new restaurants...'
+5.times do
+  Restaurant.create!(
+    name: Faker::Games::Zelda.location,
+    address: Faker::Games::StreetFighter.stage,
+    category: Restaurant::CATEGORY.sample,
+  )
+end
+puts 'HAL creation executed. Your new restaurants are ready, sir.'
